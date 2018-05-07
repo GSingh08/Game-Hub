336669e1159b6b3fc0a8a7ff2d60f2afe31c909d
Gurjot Singh <gurjotsingh@ool-ad020308.dyn.optonline.net>
2018-05-07 10:31:47 -0400

    tried changing sqlite3 to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2018_05_03_141715) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "favorites", force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_favorites_on_user_id"
